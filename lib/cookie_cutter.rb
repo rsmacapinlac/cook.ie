27f91c585153824b571ab4adc6819c178c940c8f
Ritchie Macapinlac <rsmacapinlac@boogienet.com>
2016-01-08 22:20:46 -0800

    Added logging

--- lib/cookie_cutter.rb
+++ lib/cookie_cutter.rb
@@ -1,6 +1,26 @@
-require "cookie_cutter/version"
-require "cookie_cutter/oven"
+require 'cookie_cutter/version'
+require 'cookie_cutter/oven'
+require 'log4r'
+
+include Log4r
+
+# create a logger named 'mylog' that logs to stdout
+# mylog = Logger.new 'mylog'
+# mylog.outputters = Outputter.stdout
 
 module CookieCutter
-  # Your code goes here...
+
+  class << self
+    attr_writer :logger
+
+    def logger
+      if @logger.nil?
+        @logger = Logger.new(self.name)
+        @logger.outputters = Outputter.stdout
+      end
+      @logger
+
+    end
+  end
+
 end
--- lib/cookie_cutter/oven.rb
+++ lib/cookie_cutter/oven.rb
@@ -8,17 +8,24 @@ module CookieCutter
     attr_accessor :_config, :_template_type
 
     def initialize(template_type, output_name, template_dir=ENV['CC_TEMPLATE_DIR'])
+
+      log __method__, "Loading config file..."
+
       @_config = YAML::load_file(File.join(template_dir, 'config.yml'))
       @_template_type = template_type
       @output_name = output_name
 
-      puts @_config
-      @template = File.join(template_dir, @_config[@_template_type]['template'])
-      @output_ext = get_output_file_ext(@_config[@_template_type]['template'])
+      unless @_config[@_template_type].nil?
+        log __method__, "Template:         #{@_template_type}"
+        log __method__, "Template details: #{@_config[@_template_type]}"
+        @template = File.join(template_dir, @_config[@_template_type]['template'])
+        @output_ext = get_output_file_ext(@_config[@_template_type]['template'])
+      end
     end
 
     def render()
       ERB.new(File.open(@template).read).result(binding)
+      log __method__, "Rendering cookie"
     end
 
     def save()
@@ -26,9 +33,15 @@ module CookieCutter
 
       f = File.open(output_full_path, 'w+')
       f.write(render)
+
+      log __method__, "Saving cookie to: #{output_full_path}"
     end
 
     private
+
+    def log(method_name, msg)
+      CookieCutter.logger.info "#{self.class.name}.#{method_name}: #{msg}"
+    end
 
     def get_output_file_ext(template_file)
       # assume: filename.ext.erb
